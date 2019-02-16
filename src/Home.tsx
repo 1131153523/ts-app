6ce9f27420bada5808686d06b8f840f2ccabe0a9
web小菜鸡 <38740836+1131153523@users.noreply.github.com>
2019-02-16 21:19:19 +0800

    ...

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from 'antd'
 interface IHomeProps {
     name?: string
 }
 const Home: React.SFC<IHomeProps> = ({children}) => {
     return (
         <div>
-            home
+            <Button type="primary">123456</Button>
             <br/>
             <Link to="/about">about</Link>
         </div>
